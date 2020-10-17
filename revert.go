9a2d9f97c623e3803d5ad616b7c99c900493ad2f
liyiwen <johnnyeven@gmail.com>
2020-10-17 14:36:29 +0800

    init

--- revert.go
+++ revert.go
@@ -25,23 +25,28 @@ func NewRevert() *Revert {
 	}
 }
 
-func (r *Revert) Do(funcID string, handler func() (ResponseRevertID, error)) error {
+func (r *Revert) Do(funcID string, handler func() (ResponseRevertID, error)) (err error) {
 	r.processSequence = append(r.processSequence, funcID)
 
-	resp, err := handler()
-	if err != nil {
-		// rollback
-		for _, processor := range r.processSequence {
-			if revertFunc, ok := revertFuncMapper[processor]; ok {
-				if prevResp, ok := r.processResult[processor]; ok {
-					_ = revertFunc(prevResp.GetRevertID())
-					// TODO retry
+	defer func() {
+		if err != nil {
+			// rollback
+			for _, processor := range r.processSequence {
+				if revertFunc, ok := revertFuncMapper[processor]; ok {
+					if prevResp, ok := r.processResult[processor]; ok {
+						_ = revertFunc(prevResp.GetRevertID())
+						// TODO retry
+					}
 				}
 			}
 		}
-	} else {
+	}()
+
+	var resp ResponseRevertID
+	resp, err = handler()
+	if err == nil && resp != nil {
 		r.processResult[funcID] = resp
 	}
 
-	return err
+	return
 }
